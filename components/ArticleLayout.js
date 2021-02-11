e54c3a2dcebe0de8ffa0ff2aeb79376367a98dc7
Anthony De Sousa <anthonydesousa@MacBook-Pro-de-Anthony.local>
2021-02-11 03:41:09 +0100

    finished css for Article page

--- components/ArticleLayout.js
+++ components/ArticleLayout.js
@@ -2,20 +2,42 @@ import { RichText } from "prismic-reactjs";
 import styled from "styled-components";
 
 const Layout = styled.div`
-    margin: 0;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  margin: 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h3`
+  font-size: 24px;
+  margin-top: 50px;
+`;
+
+const CreationDate = styled.p`
+  font-style: italic;
+  font-weight: light;
+`;
+
+const Paragraphe = styled.p`
+  width: 80%;
+  font-size: 20px;
+  line-height: 23px;
+  margin-bottom: 50px;
+`;
 
 export default function ArticleLayout({ data }) {
+  function formatDate(date) {
+    const publishedDate = new Date(date);
+    const options = { day: "numeric", month: "long", year: "numeric" };
+    return publishedDate.toLocaleDateString("fr-FR", options);
+  }
   return (
     <Layout>
-      <h3>{RichText.asText(data.title)}</h3>
-      <p>{data.date}</p>
+      <Title>{RichText.asText(data.title)}</Title>
+      <CreationDate>{`Publié le ${formatDate(data.date)}`}</CreationDate>
       <img src={data.image.url} alt="" height="400px" />
-      <p>{RichText.asText(data.paragraphe)}</p>
+      <Paragraphe>{RichText.asText(data.paragraphe)}</Paragraphe>
     </Layout>
   );
 }
